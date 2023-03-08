0fb2e6b1f9d6ba6ccd075b1b6ce662a135a048c8
Smuffs <campeloHP@gmail.com>
2023-03-08 18:21:01 +0000

    Changed health text size

--- Classy Clash Game/main.cpp
+++ Classy Clash Game/main.cpp
@@ -66,7 +66,7 @@ int main()
         else // Character is alive
         {
             std::string knightsHealth = "Health: ";
-            knightsHealth.append(std::to_string(knight.getHealth()), 0, 5);
+            knightsHealth.append(std::to_string(knight.getHealth()), 0, 3);
             DrawText(knightsHealth.c_str(), 55.f, 45.f, 40, RED);
         }
 
